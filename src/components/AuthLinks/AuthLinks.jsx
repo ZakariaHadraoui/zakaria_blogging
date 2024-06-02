280939aa03e24dfce61129700da5790264db2847
zakaria hadraoui <zikoxo05@gmail.com>
2024-06-02 15:17:59 +0100

    insense

--- src/components/AuthLinks/AuthLinks.jsx
+++ src/components/AuthLinks/AuthLinks.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import styles from "./authLinks.module.css";
+import styles from "./authlinks.module.css";
 import { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 
