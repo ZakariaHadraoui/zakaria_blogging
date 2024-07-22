7d0a7f6ed8173dcacf326ab3480ecefe93835f4b
zakaria hadraoui <zikoxo05@gmail.com>
2024-07-22 21:36:46 +0100

    nexti

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,7 +5,7 @@ import Pagination from '../pagination/Pagination'
 
 const getData = async (page,cat) => {
 	const res = await fetch(
-	  `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+	  `https://zakaria-blogging-ieu6.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
 	  {
 		cache: "no-store",
 	  }
