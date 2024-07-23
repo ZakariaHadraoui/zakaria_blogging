596cb871b4439f0c1ba1d34631c3644d94e91546
zakaria hadraoui <zikoxo05@gmail.com>
2024-07-23 13:41:41 +0100

    mongprisma

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,12 +8,12 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "zakblogs-c3b28.firebaseapp.com",
-  projectId: "zakblogs-c3b28",
-  storageBucket: "zakblogs-c3b28.appspot.com",
-  messagingSenderId: "505589707152",
-  appId: "1:505589707152:web:2f9b74ea49e727eb950315",
-  measurementId: "G-GY7YM8MMXB"
+  authDomain: "zakaria-vercelplat.firebaseapp.com",
+  projectId: "zakaria-vercelplat",
+  storageBucket: "zakaria-vercelplat.appspot.com",
+  messagingSenderId: "516330875442",
+  appId: "1:516330875442:web:4225df37199b35dc47aa2b",
+  measurementId: "G-DMGT1WEXK3"
 };
 
 // Initialize Firebase
